b929a56887b655a8b5fa69d89281a99abd687bfe
audreyg2020 <guo.audrey2020@gmail.com>
2024-04-04 12:00:10 -0700

    StickminHenry4

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -12,7 +12,7 @@ my_turtle.shape('turtle')
 # Set your turtle's speed using .speed(2)
 my_turtle.color('purple')
 # Set your turtle's color using .color('green') and .pencolor('blue')
-my_turtle.speed(5)
+my_turtle.speed(10)
 # Move your turtle forward using .forward(100)
 # TEST    Did your turtle move forward?
 my_turtle.forward(200)
@@ -20,16 +20,30 @@ my_turtle.forward(200)
 my_turtle.left(90)
 # Now put the forward and left/right code into a for loop to repeat 4 times.
 # TEST    Did your turtle draw a square?
-for num in range(8):
-    my_turtle.forward(200)
+for num in range(80):
+    my_turtle.forward(250)
     my_turtle.left(90)
 # Move your turtle to a new place on the screen using .goto(x, y)
 # x=0 and y=0 is the center of the screen
 my_turtle.goto(0,200)
 my_turtle.goto(200,0)
+my_turtle.goto(0,250)
+my_turtle.goto(250,0)
+my_turtle.goto(0,300)
+my_turtle.goto(300,0)
 # Have your turtle draw a circle using .circle(radius, steps=30)
 # TEST    Did your turtle draw a circle?
 my_turtle.circle(200, 360)
+my_turtle.circle(210, 360)
+my_turtle.circle(220, 360)
+my_turtle.circle(230, 360)
+my_turtle.circle(240, 360)
+my_turtle.circle(250, 360)
+my_turtle.circle(260, 360)
+my_turtle.circle(270, 360)
+my_turtle.circle(280, 360)
+my_turtle.circle(290, 360)
+my_turtle.circle(300, 360)
 # Add color to your shape by adding .begin_fill() before drawing the circle
 # and .end_fill() below
 
